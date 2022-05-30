9edd82b1548607276bf6835e2116edaf27c88fd3
Jens Shumway <jensshum@gmail.com>
2022-05-30 08:54:22 -0600

    added menu

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+function check()
+{
+    document.getElementById("button1").classList.remove("active")
+    document.getElementById("button2").classList.remove("active")
+    document.getElementById("button3").classList.remove("active")
+    return document.getElementById("button").classList.add("active");
+}
+// function change2()
+function check1()
+{
+    document.getElementById("button").classList.remove("active")
+    document.getElementById("button2").classList.remove("active")
+    document.getElementById("button3").classList.remove("active")
+    return document.getElementById("button1").classList.add("active");
+}
+// function change2()
+function check2()
+{
+    document.getElementById("button").classList.remove("active")
+    document.getElementById("button1").classList.remove("active")
+    document.getElementById("button3").classList.remove("active")
+    return document.getElementById("button2").classList.add("active");
+}
+// function change2()
+function check3()
+{
+    document.getElementById("button").classList.remove("active")
+    document.getElementById("button1").classList.remove("active")
+    document.getElementById("button2").classList.remove("active")
+    return document.getElementById("button3").classList.add("active");
+}
